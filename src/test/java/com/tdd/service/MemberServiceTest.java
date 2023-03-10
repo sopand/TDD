3f6157cb75e116e10244c3dced9cb99081bd4e2f
sopand <pows1031@naver.com>
2023-03-10 23:52:51 +0900

    03-10 Junit MemberService-Create ( Mock 사용 )

--- src/test/java/com/tdd/service/MemberServiceTest.java
+++ src/test/java/com/tdd/service/MemberServiceTest.java
@@ -4,33 +4,43 @@ import com.tdd.dto.MemberRequest;
 import com.tdd.dto.MemberResponse;
 import com.tdd.entity.MemberRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
 @Transactional
+@ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
+    //Mock = 익명 클래스 느낌( 가짜 객체 생성 )
+    @InjectMocks
+    private MemberService memberService;
 
-    @Autowired
+    @Mock
     private MemberRepository memberRepository;
 
+
     @Test
-    void createMember(){
+    public void createMember(){
         //given
         MemberRequest member=new MemberRequest();
-        String email="d";
-        String pwd="d";
-        member.setEmail(email);
-        member.setPwd(pwd);
-        //when
-        MemberService service=new MemberService(memberRepository);
-        MemberResponse memberResponse=service.createMember(member);
-        //then
-        assertEquals(member.getEmail(),memberResponse.getEmail());
-        assertEquals(member.getPwd(),memberResponse.getPwd());
+        member.setEmail("d");
+        member.setPwd("d");
 
+        //stub ( 행동 정의 )
+        //when(memberRepository.save(member.memberEntity())).thenReturn(member.memberEntity());
+        when(memberRepository.save(any())).thenReturn(member.memberEntity());
+
+        //when
+        MemberResponse memberResponse=memberService.createMember(member);
+        //then
+        assertThat(member.getEmail()).isEqualTo(memberResponse.getEmail());
+        assertThat(member.getPwd()).isEqualTo(memberResponse.getPwd());
     }
 }
